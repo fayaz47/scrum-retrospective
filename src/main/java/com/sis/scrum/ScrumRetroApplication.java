b5c6cc7e0a47a4d44da507b0114dbe312c146f96
C0281566 <Mahammad.Fayaz@santander.co.uk>
2023-09-23 07:55:12 +0100

    First checkin

---
+++ src/main/java/com/sis/scrum/ScrumRetroApplication.java
@@ -0,0 +1,13 @@
+package com.sis.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumRetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumRetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sis/scrum/ScrumretroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sis.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumretroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
